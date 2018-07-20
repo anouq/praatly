a77447650ebafcf946795ef0a1858c8b67b77a5c
anouq <anouq>
2018-07-20 20:01:48 +0200

    Generating stats map

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as _ from 'lodash';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-menu',
@@ -7,6 +9,8 @@ import { Component } from '@angular/core';
 })
 export class MenuComponent {
 
+  private static readonly TYPE = 'type';
+  private all = [];
   private rawPraat = [];
 
   public uploadPraat(input: any): void {
@@ -15,14 +19,15 @@ export class MenuComponent {
       const reader: FileReader = new FileReader();
       reader.onload = () => {
         let date = input.files[i].name.substr(6, 11);
-        if (date.endsWith('_')) {
+        if (date.endsWith('_')) { // For days 1-9
           date = date.slice(0, -1);
         }
-
         this.rawPraat.push(this.convertPraat(reader.result, date));
       };
       reader.readAsText(input.files[i]);
     }
+
+    setTimeout(() => this.generateStatsMap(), 100);
   }
 
   private convertPraat(file: any, date: string): void {
@@ -30,7 +35,7 @@ export class MenuComponent {
     return lines
       .filter(line => typeof line[0] !== 'undefined')
       .map(line => {
-        return `date,${first}`
+        return `date,${first}` // Manually add date column
           .split(',')
           .reduce((curr, next, i) => ({
             ...curr, [next]: `${date},${line}`.split(',')[i]
@@ -38,4 +43,34 @@ export class MenuComponent {
       });
   }
 
+  private generateStatsMap(): void {
+    this.all = [];
+    this.rawPraat.forEach((file) => {
+      file.forEach((recording) => {
+        recording[MenuComponent.TYPE] = recording[MenuComponent.TYPE].trim().replace(/\W+/g, '_');
+        ['minPitch', 'meanPitch', 'maxPitch', 'n', 'sec'].forEach(key => recording[key] = Number(recording[key]));
+        this.all.push(recording);
+      });
+    });
+
+    const types = [null, ...this.all // null represents total
+      .map(item => item[MenuComponent.TYPE])
+      .filter((value, i, t) => t.indexOf(value) === i)];
+
+    types.forEach((type) => {
+      // If type of recording type is null, use all recordings; otherwise, split recordings by type
+      const byType = type === null ? this.all : _.groupBy(this.all, MenuComponent.TYPE)[type];
+
+      const statsMap = _.chain(byType)
+        .groupBy('date')
+        .map((byDate, key) => ({
+          date: moment(key, 'YYYY-MMM-D').format('YYYY-MM-DD'),
+          minPitch: _.ceil(_.minBy(byDate, 'minPitch')['minPitch']),
+          maxPitch: _.ceil(_.maxBy(byDate, 'maxPitch')['maxPitch']),
+          minutes: _.ceil(_.sumBy(byDate, 'sec') / 60),
+          recordings: byDate.length
+        }))
+        .value();
+    });
+  }
 }
