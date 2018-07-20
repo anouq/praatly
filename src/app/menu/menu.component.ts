b3556ac001bfeb2c4bcc482ddbee1f23e2acaef7
anouq <anouq>
2018-07-20 16:25:40 +0200

    Upload Praat output and convert to objects

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -6,4 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent {
+
+  private rawPraat = [];
+
+  public uploadPraat(input: any): void {
+    this.rawPraat = [];
+    for (let i = 0; i < input.files.length; i++) {
+      const reader: FileReader = new FileReader();
+      reader.onload = () => {
+        let date = input.files[i].name.substr(6, 11);
+        if (date.endsWith('_')) {
+          date = date.slice(0, -1);
+        }
+
+        this.rawPraat.push(this.convertPraat(reader.result, date));
+      };
+      reader.readAsText(input.files[i]);
+    }
+  }
+
+  private convertPraat(file: any, date: string): void {
+    const [first, ...lines] = file.split('\n');
+    return lines
+      .filter(line => typeof line[0] !== 'undefined')
+      .map(line => {
+        return `date,${first}`
+          .split(',')
+          .reduce((curr, next, i) => ({
+            ...curr, [next]: `${date},${line}`.split(',')[i]
+          }), {});
+      });
+  }
+
 }
